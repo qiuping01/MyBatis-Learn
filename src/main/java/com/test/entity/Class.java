4a0f4437db66fd65b163cab24c42e2040fb815d9
qiuping01 <2187718660@qq.com>
2025-07-18 23:27:34 +0800

    开启二级缓存

--- src/main/java/com/test/entity/Class.java
+++ src/main/java/com/test/entity/Class.java
@@ -1,8 +1,9 @@
 package com.test.entity;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class Class {
+public class Class implements Serializable {
     private Integer id;
     private String name;
     private List<Student> student;
